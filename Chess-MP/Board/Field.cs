92838e4d087eb5e0d6260579a41448242e51f416
sebathefox <seba4928@edu.sde.dk>
2020-10-12 14:27:24 +0200

    Added Field.

--- Chess-MP/Board/Field.cs
+++ Chess-MP/Board/Field.cs
@@ -2,15 +2,25 @@ using Chess_MP.Pieces;
 
 namespace Chess_MP.Board
 {
+    /**
+     * Contains the logic for fields.
+     * @author Sebastain Davaris
+     * @date 12-10-2020
+     */
     public class Field
     {
-        private IPiece _occupant;
+        private IPiece _piece;
 
         public Field()
         {
-            _occupant = null;
+            _piece = null;
         }
 
-        public IPiece Piece => _occupant;
+        /**
+         * Gets the piece currently on the field.
+         * @author Sebastian Davaris
+         * @date 12-10-2020
+         */
+        public IPiece Piece => _piece;
     }
 }
