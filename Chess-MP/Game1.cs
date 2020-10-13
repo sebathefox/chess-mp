2e520ad26ce15950835c07ad9297de22952160f6
sebathefox <seba4928@edu.sde.dk>
2020-10-13 09:36:50 +0200

    Added dummy image.

--- Chess-MP/Game1.cs
+++ Chess-MP/Game1.cs
@@ -11,6 +11,8 @@ namespace Chess_MP
         private SpriteBatch _spriteBatch;
         private AssetManager _assetManager;
 
+        private Image _image;
+
         public event EventHandler<SpriteBatch> OnDraw; 
 
         public Game1()
@@ -35,6 +37,8 @@ namespace Chess_MP
             _assetManager.LoadTexture("board", "board");
             _assetManager.LoadTexture("white-queen", "white_queen");
 
+            _image = new Image(this, _assetManager.GetTexture("white-queen"), Vector2.One);
+            
             // TODO: use this.Content to load your game content here
         }
 
@@ -44,6 +48,18 @@ namespace Chess_MP
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            KeyboardState state = Keyboard.GetState();
+            
+            if(state.IsKeyDown(Keys.W))
+                _image.SetPosition(new Vector2(_image.Position.X, _image.Position.Y - 1));
+            if(state.IsKeyDown(Keys.S))
+                _image.SetPosition(new Vector2(_image.Position.X, _image.Position.Y + 1));
+            if(state.IsKeyDown(Keys.A))
+                _image.SetPosition(new Vector2(_image.Position.X - 1, _image.Position.Y));
+            if(state.IsKeyDown(Keys.D))
+                _image.SetPosition(new Vector2(_image.Position.X + 1, _image.Position.Y));
+            
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
