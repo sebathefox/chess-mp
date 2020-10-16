0822516e145b310cf10c6a438f06ead30c8aa36b
Kasper Grøn Madsen <kasper.krumme.madsen@gmail.com>
2020-10-16 08:40:24 +0200

    Button beginning
    Undskyld sebastian

---
+++ Chess-MP/Button.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Chess-MP
+{
+    class Button
+    {
+        int buttonX, buttonY;
+
+        public int ButtonX
+        {
+            get
+            {
+                return buttonX;
+            }
+        }
+
+        public int ButtonY
+        {
+            get
+            {
+                return buttonY;
+            }
+        }
+
+        public Button(string name, Texture2D texture, int buttonX, int buttonY)
+        {
+            this.Name = name;
+            this.Texture = texture;
+            this.buttonX = buttonX;
+            this.buttonY = buttonY;
+        }
+    }
+}
